1aab665e41885e68bfff69f009d3286c06889b3f
Heidi Hlavinka <hchlav@gmail.com>
2022-06-12 21:37:09 -0500

    commit tree 19a927847a56f48e0f79b19a0e0067943e2ab2bc

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  #your code here
+
+  attr_accessor :name
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end  
+  end
+  
 end
